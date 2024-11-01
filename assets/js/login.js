bd3b664bd6851e472035c79880231f37afb28d99
Irham Baehaqi <149279763+IrhamBae@users.noreply.github.com>
2024-11-02 04:58:47 +0700

    Create login.js

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+// Fungsi untuk pindah ke halaman Search menggunakan jQuery
+function Search() {
+    const username = $('#username').val();
+    const password = $('#password').val();
+
+    if (username === "" || password === "") {
+        alert("Username dan Password tidak boleh kosong!");
+    } else {
+        // Pindah ke halaman Search.html
+        window.location.href = 'Search.html';
+    }
+}
