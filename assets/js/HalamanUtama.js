016efddced08e9097ab580b73457a1c6d4b9fa04
FaqihAinulyaqin <mfaqihay@gmail.com>
2024-11-05 16:23:38 +0700

    Add Script and json for data

---
+++ assets/js/HalamanUtama.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    $.getJSON('assets/json/product.json', function(data) {
+        let rows = '';
+        let rowCount = 0;
+
+        $.each(data, function(index, item) {
+            if (rowCount === 0) {
+                rows += '<tr>';
+            }
+
+            rows += '<td>' +
+                '<div class="product-item">' +
+                '<i class="far fa-heart favorite" onclick="toggleFavorite(this)"></i>' +
+                '<div class="product-image">'+
+                '<img alt="Product Image" height="200" src="'+ item.image +'" width="150" />' +
+                '</div>' +
+                '<div class="info">' +
+                '<div class="name">'+ item.name +'</div>' +
+                '<div class="price">'+ item.price +'</div>' +
+                '<div class="description">'+ item.description +'</div>' +          
+                '</div>' +
+                '</div>' +
+                '</td>';
+
+            rowCount++;
+
+            if (rowCount === 4) {
+                rows += '</tr>';
+                rowCount = 0;
+            }
+        });
+
+        if (rowCount > 0) {
+            rows += '</tr>';
+        }
+
+        $('#data-table').append(rows);
+    });
+});
+
+function toggleFavorite(icon) {
+    $(icon).toggleClass('fas far');
+}
